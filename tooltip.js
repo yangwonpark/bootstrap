6c166cc4341dc3968d0f66500bc3a599b4ded9f2
yangwonpark <ailey@nate.com>
2021-05-25 14:44:41 +0900

    5. bootstrap tooltips

---
+++ tooltip.js
@@ -0,0 +1,5 @@
+// tooltip 초기화 코드
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
